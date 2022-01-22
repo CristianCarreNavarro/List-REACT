dc6b389cf036708a8bf343903dbd30fcaf5f0402
CristianCarreNavarro <chris_carre@hotmail.com>
2022-01-22 11:22:08 +0100

    Creating Project
    Add alls packages

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { ImprLiteral } from "./App";
+import { App } from "./App";
+
+ReactDOM.render(<ImprLiteral />, document.getElementById("root"));
+/*ReactDOM.render(<div>Hola Mundo</div>, document.getElementById("root"));*/
+ReactDOM.render(<App />, document.getElementById("root"));
